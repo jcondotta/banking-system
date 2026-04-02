fd0018d9cede4c939e55da323545fe6ea5c2490e
jcondotta <jefferson.condotta@gmail.com>
2026-04-02 15:58:14 +0200

    Add Instancio dependency for improved testing support

--- bank-accounts-messaging/src/main/java/com/jcondotta/bankaccounts/notification/BankAccountActivatedEvent.java
+++
@@ -1,6 +0,0 @@
-package com.jcondotta.bankaccounts.notification;
-
-import java.util.UUID;
-
-public record BankAccountActivatedEvent(UUID bankAccountId, String occurredAt) {
-}
--- bank-accounts-messaging/src/main/java/com/jcondotta/bankaccounts/notification/EmailSender.java
+++
@@ -1,6 +0,0 @@
-package com.jcondotta.bankaccounts.notification;
-
-public interface EmailSender {
-
-  void send(String to, String subject, String body);
-}
